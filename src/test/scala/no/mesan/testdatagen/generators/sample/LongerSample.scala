64d275cf9ff93527ac9acba838699512fcf5c0ab
Lars Reed <lars.reed@mesan.no>
2013-02-26 22:25:05 +0100

    Expanded longer sample

--- src/test/scala/no/mesan/testdatagen/generators/sample/LongerSample.scala
+++ src/test/scala/no/mesan/testdatagen/generators/sample/LongerSample.scala
@@ -7,19 +7,25 @@ import no.mesan.testdatagen.generators.norway.{Adresser, Fnr, Poststeder}
 import no.mesan.testdatagen.recordgen.ToSql
 
 object LongerSample extends App {
+  // These are the total of numbers we will generate for the different categories
   val totalOrders= 200
   val totalProducts= totalOrders/2
   val totalCustomers= totalOrders/3
   val totalOrderLines= totalOrders*3
 
+  // We generate one script for all data
   val resultFile= "orders.sql"
 
+  // To be able to reuse values between records, we generate some values in advance,
+  // specifically IDs (for foreign keys) and dates (for correlation between birth dates
+  // and "fodselsnummer"
   val customerIds= FromList(Ints().from(1).to(10000).unique.get(totalCustomers))
   val birthDates= Dates() from (y=1921) to (y=1996) get(totalCustomers)
   val productIds= FromList(Ints().from(1).to(100000).unique.get(totalProducts))
   val orderIds= FromList(Ints().from(1).to(1000).unique.get(totalOrders)) sequential
   val postSteder= FromList(Poststeder() get(totalCustomers)) sequential
 
+  // Populating the customer table - no dependencies
   val customerGenerator= 
     ToSql("customer")
     .add("id", customerIds)
@@ -29,6 +35,7 @@ object LongerSample extends App {
     .addQuoted("postnr", TextWrapper(postSteder).substring(0, 4))
     .addQuoted("poststed", TextWrapper(postSteder).substring(5))
 
+  // and products - no dependencies either
   val productGenerator=
     ToSql("product")
     .add("id", productIds)
@@ -36,6 +43,7 @@ object LongerSample extends App {
                          .add(60, Names(1))
                          .add(40, Names(2)))
 
+  // Orders are connected to customers through customerIds
   val orderGenerator=
     ToSql("order")
     .add("id", orderIds)
@@ -43,6 +51,7 @@ object LongerSample extends App {
     .add("customer", customerIds)
     .addQuoted("orderDate", Dates() from(y=2010) to(y=2013) format("yyyy-MM-dd"))
 
+  // And order_lines connected to orders and products
   val orderLineGenerator=
     ToSql("order_line")
     .add("order", orderIds)
@@ -59,6 +68,7 @@ object LongerSample extends App {
                         .add(FromList(" l", " kg", "", " m")))
                         .trim)))
 
+  // The generators are all set -- create result
   customerGenerator toFile(resultFile) get(totalCustomers)
   productGenerator appendToFile(resultFile) get(totalProducts)
   orderGenerator appendToFile(resultFile) get(totalOrders)

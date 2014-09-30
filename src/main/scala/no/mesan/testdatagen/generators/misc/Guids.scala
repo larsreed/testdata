45add968d82f6bd975ed9970acc5d6ca0a0a102c
Lars Reed <lars.reed@mesan.no>
2014-09-30 07:33:40 +0200

    clean up, guid

--- src/main/scala/no/mesan/testdatagen/generators/misc/Guids.scala
+++ src/main/scala/no/mesan/testdatagen/generators/misc/Guids.scala
@@ -18,8 +18,9 @@ class Guids extends GeneratorImpl[Seq[Long]] with StreamGeneratorImpl[Seq[Long]]
   private val p4Gen= Longs() from 0
 
   def getStream: Stream[Seq[Long]]=
-    p1Gen.gen.zip(p2Gen.gen).zip(p3Gen.gen).zip(p4Gen.gen).map(v=>
-      Seq(v._1._1._1, v._1._1._2, v._1._2, v._2)) // ugly, but works...
+    p1Gen.gen.zip(p2Gen.gen).zip(p3Gen.gen).zip(p4Gen.gen).map {
+      case (((a, b), c), d) => Seq(a, b, c, d)
+    }
 
   /** get values as BigInts rather than tuples. */
   def getBigInts: Stream[BigInt] = genStrings map (s=> BigInt(s.replaceAll("-", ""), 16))
--- src/test/scala/no/mesan/testdatagen/generators/misc/GuidsSuite.scala
+++ src/test/scala/no/mesan/testdatagen/generators/misc/GuidsSuite.scala
@@ -9,8 +9,6 @@ class GuidsSuite extends FlatSpec {
 
   "The Guids" should "produce correctly formatted strings" in {
     Guids().getStrings(120).foreach(s =>
-      println(s))
-    Guids().getStrings(120).foreach(s =>
       assert(s.matches("^[0-9a-f]{8}-([0-9a-f]{4}-){2}[0-9a-f]{16}$"), s))
   }
 }
